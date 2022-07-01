b35b94990031db592960f9e9b0bd5d57527d96bf
DeylerAF <deylerafernandez@gmail.com>
2022-07-01 01:39:58 -0600

    Starting Using thunks to create server resources

--- src/todos/reducers.js
+++ src/todos/reducers.js
@@ -19,37 +19,43 @@ export const isLoading = (state = false, action) => {
         default:
             return state;
     }
-}
+};
 
 export const todos = (state = [], action) => {
     const { type, payload } = action;
 
     switch (type) {
-    case CREATE_TODO: {
-        const { text } = payload;
-        const newTodo = { 
-            text, 
-            isCompleted: false
-        };
-        return state.concat(newTodo);
+        case CREATE_TODO: {
+            const { text } = payload;
+            const newTodo = {
+                text,
+                isCompleted: false,
+            };
+            return state.concat(newTodo);
+        }
+        case REMOVE_TODO: {
+            const { text } = payload;
+            return state.filter((todo) => todo.text !== text);
+        }
+        case MARK_TODO_AS_COMPLETED: {
+            const { text } = payload;
+            return state.map((todo) => {
+                if (todo.text === text) {
+                    return {
+                        ...todo,
+                        isCompleted: true,
+                    };
+                }
+                return todo;
+            });
+        }
+        case LOAD_TODOS_IN_PROGRESS: {
+            const { todos } = payload;
+            return todos;
+        }
+        case LOAD_TODOS_SUCCESS:
+        case LOAD_TODOS_FAILURE:
+        default:
+            return state;
     }
-    case REMOVE_TODO: {
-        const { text } = payload;
-        return state.filter(todo => todo.text !== text);
-    }
-    case MARK_TODO_AS_COMPLETED: {
-        const { text } = payload;
-        return state.map(todo => {
-            if (todo.text === text) {
-                return {
-                    ...todo,
-                    isCompleted: true
-                };
-            }
-            return todo;
-        });
-    }
-    default:
-        return state;
-    }
-}
+};
